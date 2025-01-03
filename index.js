dd536f479bbe2d0c2ebcd620d6febce48c85844a
Sprees <alexrminnick@gmail.com>
2025-01-03 12:48:03 -0700

    MVP working as intended

--- index.js
+++ index.js
@@ -1,66 +1,68 @@
-let options = ['rock', 'paper', 'scissors']
+const btns = document.querySelectorAll('.weapon')
+let playerScore = 0, computerScore = 0
+// CONSTRUCT HTML
+const container = document.querySelector('.container')
+const gameInfoDiv = document.querySelector('.game-info')
+const playerScoreDisplay = document.getElementById('player-score')
+const computerScoreDisplay = document.getElementById('computer-score')
+const commentDisplay = document.getElementById('comment')
+const playBtn = document.getElementById('play')
+
+btns.forEach((btn) => {
+    btn.addEventListener('click', (e) => playRound(e.target.innerText, getComputerChoice()))
+})
+playBtn.addEventListener('click', () => {
+    if(playerScore > 0 || computerScore > 0) {
+        commentDisplay.innerText = "Choosing to play again, let's do this!"
+        playerScoreDisplay.innerText = 0
+        computerScoreDisplay.innerText = 0
+        playerScore = 0
+        computerScore = 0
+        btns.forEach(btn => btn.disabled = false)
+    }
+    playBtn.setAttribute('class', 'hide')
+    btns.forEach(btn => btn.removeAttribute('class', 'hide'))
+})
+
 
 function getComputerChoice() {
+    let options = ['rock', 'paper', 'scissors']
     let randomNum = Math.floor(Math.random() * 3)
     return options[randomNum]
 }
 
-function getHumanChoice() {
-    let input = prompt('Rock, Paper, or Scissors?')
-    input = input.toLowerCase();
-    
-    if(options.includes(input)) return input
-    console.log('Please type a valid option.')
-    return getHumanChoice()
-}
-
-
-(function playGame() {
-    let roundCount = 5
-    let humanScore = 0, computerScore = 0
-    
-    console.log(
-        "Let's play a game of Rock, Paper, Scissors!",
-        "After 5 rounds, the highest score wins!"
-    )
-    
-    function playRound(humanChoice, computerChoice) {
-        if(
-            humanChoice === 'rock' && computerChoice === 'paper' ||
-            humanChoice === 'paper' && computerChoice === 'scissors' ||
-            humanChoice === 'scissors' && computerChoice === 'rock'
-        ) {
-            console.log(`You lose! ${computerChoice} beats ${humanChoice}.`)
-            computerScore += 1
-        } else if(
-            humanChoice === 'rock' && computerChoice === 'scissors' ||
-            humanChoice === 'paper' && computerChoice === 'rock' ||
-            humanChoice === 'scissors' && computerChoice === 'paper'
-        ) {
-            console.log(`You win! ${humanChoice} beats ${computerChoice}.`)
-            humanScore += 1
+function playRound(humanChoice, computerChoice) {
+    if (
+        humanChoice === 'rock' && computerChoice === 'paper' ||
+        humanChoice === 'paper' && computerChoice === 'scissors' ||
+        humanChoice === 'scissors' && computerChoice === 'rock'
+    ) {
+        computerScore += 1
+        computerScoreDisplay.innerText = computerScore
+        if(computerScore < 5) {
+            commentDisplay.innerText = `You lose this round! ${computerChoice} beats ${humanChoice}`
         } else {
-            console.log("It's a draw!")
+            btns.forEach(btn => btn.disabled = true)
+            commentDisplay.innerText = `You lose the game! Play again?`
+            playBtn.innerText = 'Play again'
+            playBtn.removeAttribute('class', 'hide')
         }
-        // console.log(`Player Score: ${humanScore} Computer Score: ${computerScore}`)
-    }
-
-    while(roundCount > 0) {
-        playRound(humanSelection = getHumanChoice(), computerSelection = getComputerChoice())
-        roundCount--
-    }
-
-    if(humanScore > computerScore) {
-        console.log(`You win the game! ${humanScore} to ${computerScore}`)
-    } else if(humanScore < computerScore) {
-        console.log(`You lose the game! ${humanScore} to ${computerScore}`)
+    } else if (
+        humanChoice === 'rock' && computerChoice === 'scissors' ||
+        humanChoice === 'paper' && computerChoice === 'rock' ||
+        humanChoice === 'scissors' && computerChoice === 'paper'
+    ) {
+        playerScore += 1
+        playerScoreDisplay.innerText = playerScore
+        if(playerScore < 5) {
+            commentDisplay.innerText = `You win this round! ${humanChoice} beats ${computerChoice}`
+        } else {
+            btns.forEach(btn => btn.disabled = true)
+            commentDisplay.innerText = `You win the game! Play again?`
+            playBtn.innerText = 'Play again'
+            playBtn.removeAttribute('class', 'hide')
+        }
     } else {
-        console.log(`You've ended the game in a draw. ${humanScore} to ${computerScore}`)
+        commentDisplay.innerText = "It's a draw!"
     }
-    
-    if(confirm('Play again?')) {
-        playGame()
-    }
-
-    console.log('Thanks for playing!')
-})()
+}
